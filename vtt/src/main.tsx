1f8c27a8b6a4e27aefed7978b8d40e459319e7d6
andrewcook <andrewjcook93@gmail.com>
2023-10-01 10:49:45 -0500

    set up react router, createBrowserRouter

--- vtt/src/main.tsx
+++ vtt/src/main.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import Home from './containers/Home.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route path='/' element={<Home />}></Route>
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
