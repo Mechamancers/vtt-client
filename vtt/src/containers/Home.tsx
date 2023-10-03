acec87dde807538518cdf85da93e4863b047939b
andrewcook <andrewjcook93@gmail.com>
2023-10-02 20:54:11 -0500

    docs: notes added to Home.tsx

--- vtt/src/containers/Home.tsx
+++ vtt/src/containers/Home.tsx
@@ -1,9 +1,15 @@
 const Home=() => {
     return (
         <div className='h-[100svh] w-[100svw] bg-blackish flex items-center justify-center'>
+            {/* Header */}
             <header className='h-[20rem] flex flex-col justify-center content-center text-center'>
-                <span className='block font-title text-5xl text-center text-platinum'>Mechamancers</span>
-                <span className='block font-title text-3xl mt-2 text-center text-platinum'>Intuitive Tabletop</span>
+                {/* Heading - Primary */}
+                <span className='block font-title text-5xl text-center text-platinum'>
+                    Mechamancers</span>
+                {/* Headig - Secondary */}
+                <span className='block font-title text-3xl mt-2 text-center text-platinum'>
+                    Intuitive Tabletop</span>
+                {/* Authentication Button (Google) - Opens Modal */}
                 <button className='mt-8 bg-white'>Sign In / Sign Up</button>
             </header>
         </div>
