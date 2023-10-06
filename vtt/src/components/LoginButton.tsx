39c8a4da6226210204a1792149942db7de35bc2d
andrewcook <andrewjcook93@gmail.com>
2023-10-05 21:26:09 -0500

    fix: rm extra tag on react icon - google button

--- vtt/src/components/LoginButton.tsx
+++ vtt/src/components/LoginButton.tsx
@@ -9,7 +9,7 @@ const LoginButton=() =>{
     return (
         <button onClick={() => login()}  
             className='p-2 w-[20rem] bg-google font-google rounded-lg  text-white text-lg flex justify-center items-center '>
-            <><BsGoogle /></>
+            <BsGoogle />
             <div className='pl-4'>Continue with Google</div>
         </button>
     )
