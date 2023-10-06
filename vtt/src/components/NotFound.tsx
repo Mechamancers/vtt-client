6020f351a6c63874faa5066aa6746de621a3a1a5
andrewcook <andrewjcook93@gmail.com>
2023-10-06 09:55:43 -0500

    style: clickable logo on NotFound

--- vtt/src/components/NotFound.tsx
+++ vtt/src/components/NotFound.tsx
@@ -1,10 +1,14 @@
 import { Link } from 'react-router-dom'
+import { GiMagicPortal } from 'react-icons/gi'
 
 const NotFound=() =>{
     return(
-        <div className='bg-blackish h-[100svh] w-[100svw] flex justify-center items-center'>
+        <div className='bg-blackish h-[100svh] w-[100svw] flex flex-col justify-center items-center text-platinum'>
+            <Link to='/' className='mb-2'>
+                <GiMagicPortal style={{ fontSize: '2.5rem' }}/>
+            </Link>
             <div className='flex '>
-                <span className='text-platinum mb-20'>Page Not Found. </span>
+                <span className='mb-10'>Page Not Found. </span>
                 &nbsp;
                 <Link to='/' className='font-sans text-platinum'> Return Home</Link>
             </div>
